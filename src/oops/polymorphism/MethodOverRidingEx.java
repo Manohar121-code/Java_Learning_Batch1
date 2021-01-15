9543dbfadd640b77700f8db26da029693bd82c30
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2021-01-15 19:42:56 +0530

    overriding example

--- src/oops/polymorphism/MethodOverRidingEx.java
+++ src/oops/polymorphism/MethodOverRidingEx.java
@@ -1,11 +1,26 @@
 package oops.polymorphism;
 
+import java.util.Scanner;
+
 public class MethodOverRidingEx {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		System.out.println("main started");
 		Child c1 = new Child();
 		c1.test();
 		c1.test2();
 		c1.test4();
 		c1.test5();
+		
+		
+		int n = new Scanner(System.in).nextInt();
+		
+		if (1 <= n && n <= 10) {
+			System.out.println("if");
+		} else {
+			System.out.println("else");
+		}
+		
+		System.out.println("main ended");
 	}
 }
